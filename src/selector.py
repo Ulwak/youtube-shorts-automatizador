6265e2b221177a01448488819ac6c030de535ce4
Ulwak <uliseslazzarini@gmail.com>
2026-05-12 23:47:50 -0300

    refactor: generalizar seleccion y movimiento de archivos

--- src/selector.py
+++ src/selector.py
@@ -1,6 +1,14 @@
 import random
 from pathlib import Path
 
+def seleccionar_archivo(ruta):
+    archivo = Path(ruta)
+    archivo_2 = [n for n in archivo.iterdir() if n.is_file()]
+    return random.choice(archivo_2)
+
+def mover_archivo(ruta, archivo):
+    archivo.rename(Path(ruta / "usados" / archivo.name))
+
 def seleccionar_categoria():
     contenido = Path(__file__).parent.parent / "memes" / "disponibles"
     carpeta = [n for n in contenido.iterdir() if n.is_dir()]
@@ -24,12 +32,25 @@ def revisar_si_hay_memes_disponibles(carpeta, memes):
             return seleccionar_memes(carpeta)
 
 def main():
+    ruta_musica = Path(__file__).parent.parent / "musica"
+    ruta_fondo = Path(__file__).parent.parent / "fondos"
+    ruta_like = Path(__file__).parent.parent / "likes"
+    ruta_comentarios = Path(__file__).parent.parent / "comentarios"
     carpeta = seleccionar_categoria()
+    fondo = seleccionar_archivo(ruta_fondo / "disponibles")
+    like = seleccionar_archivo(ruta_like / "disponibles")
+    musica = seleccionar_archivo(ruta_musica / "disponibles")
+    comentarios = seleccionar_archivo(ruta_comentarios / "disponibles")  
     try:
         memes = seleccionar_memes(carpeta)
     except:
-       memes = revisar_si_hay_memes_disponibles(carpeta, memes)
+        memes = revisar_si_hay_memes_disponibles(carpeta, memes)
     mover_memes_a_usados(memes)
+    mover_archivo(ruta_fondo, fondo)
+    mover_archivo(ruta_like, like)
+    mover_archivo(ruta_musica, musica)
+    mover_archivo(ruta_comentarios, comentarios)
+    return memes, fondo, like, musica, comentarios
 
 if __name__ == "__main__":
     main()
