ce5336522655acc28ef39956afc5ea145600c900
Ulwak <uliseslazzarini@gmail.com>
2026-05-12 23:09:41 -0300

    feat:agregar seleccion, movimiento y revision de memes, agregar carpetas de like y comentarios

--- src/selector.py
+++ src/selector.py
@@ -3,5 +3,33 @@ from pathlib import Path
 
 def seleccionar_categoria():
     contenido = Path(__file__).parent.parent / "memes" / "disponibles"
-    carpetas = [n for n in contenido.iterdir() if n.is_dir()]
-    return random.choice(carpetas)
+    carpeta = [n for n in contenido.iterdir() if n.is_dir()]
+    return random.choice(carpeta)
+
+def seleccionar_memes(carpeta):
+    memes = [n for n in carpeta.iterdir() if n.is_file()]
+    return random.sample(memes, 2)
+
+def mover_memes_a_usados(memes):
+    for meme in memes:
+        categoria = meme.parent.name
+        meme.rename(Path(__file__).parent.parent / "memes" / "usados" / categoria / meme.name)
+
+def revisar_si_hay_memes_disponibles(carpeta, memes):
+    if not memes:
+            memes_usados = Path(__file__).parent.parent / "memes" / "usados" / carpeta.name
+            usados = [n for n in memes_usados.iterdir() if n.is_file()]
+            for usado in usados:
+             usado.rename(Path(__file__).parent.parent / "memes" / "disponibles" / carpeta.name / usado.name)
+            return seleccionar_memes(carpeta)
+
+def main():
+    carpeta = seleccionar_categoria()
+    try:
+        memes = seleccionar_memes(carpeta)
+    except:
+       memes = revisar_si_hay_memes_disponibles(carpeta, memes)
+    mover_memes_a_usados(memes)
+
+if __name__ == "__main__":
+    main()
